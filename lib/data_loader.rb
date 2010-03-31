a072096e0963aa7a654db8bee53dfea80d43608b
robmckinnon <rob@theyworkforyou.co.nz>
2010-03-31 10:45:42 +0100

    initial work on data loader

---
+++ lib/data_loader.rb
@@ -0,0 +1,73 @@
+require 'roo'
+require 'morph'
+
+class FundRecord
+  include Morph
+end
+
+class DataLoader
+
+  def row_not_empty(s, row)
+    s.cell(row,1) ? true : false
+  end
+
+  def convert excel_file
+    sheet = Excel.new(excel_file)
+    raise 'expected value in first cell' unless row_not_empty(sheet, 1)
+    FasterCSV.generate do |csv|
+      1.upto(sheet.last_row) do |row_index|
+        row = []
+        1.upto(sheet.last_column) do |col|
+          row << sheet.cell(row_index, col)
+        end
+        csv << row
+      end
+    end
+  end
+
+  def convert_to_morph_method_name label
+    name = label.to_s.downcase.tr('()\-*',' ').gsub('%','percentage').strip.chomp(':').strip.gsub(/\s/,'_').squeeze('_')
+    name = '_'+name if name =~ /^\d/
+    name.gsub!(/[^a-z]/,'_')
+    name
+  end
+
+  def load_fund_files file_name
+    csv = IO.read(file_name)
+    csv.sub!('Excel/PDF','Excel_or_PDF')
+    Morph.from_csv csv, 'FundFile'
+  end
+
+  def field_names fund_file
+    attributes = fund_file.class.morph_attributes
+    fields = attributes.select{|a| a.to_s[/_field$/]}
+    fields.collect do |field|
+      normalized = field.to_s.sub(/_field$/,'').to_sym
+      original = convert_to_morph_method_name(fund_file.send(field)).to_sym
+      [normalized, original]
+    end
+  end
+
+  def load_fund_file fund_file
+    name = fund_file.parsed_data_file
+    country_code = name[0..1]
+    file_name = "#{RAILS_ROOT}/data/#{country_code}/#{name}"
+    csv = IO.read(file_name)
+    raw_records = Morph.from_csv csv, 'RawRecord'
+    
+    field_names = field_names(fund_file)
+    
+    raw_records.collect do |raw|
+      record = FundRecord.new
+      field_names.each do |field|
+        normalized = field[0]
+        original = field[1]
+        raise raw.inspect
+        value = raw.send(original)
+        record.morph(normalized, value)
+      end
+      record
+    end
+  end
+
+end
---
+++ spec/lib/data_loader_spec.rb
@@ -0,0 +1,92 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe DataLoader do
+  
+  before :each do
+    @loader = DataLoader.new
+  end
+
+  it 'should convert xls to csv' do
+    converted = @loader.convert(RAILS_ROOT+'/data/pl_in_progress_erdf.xls')
+    converted.should == pl_csv
+  end
+
+  it 'should create method name without unicode' do
+    name = 'Tytuł projektu'
+    converted = @loader.convert_to_morph_method_name name
+    converted.should == 'tytu__projektu'
+  end
+
+  it 'should load CSV' do
+    fund_file = fund_files.first
+    fund_file.class.name.should == 'Morph::FundFile'
+    fund_file.country.should == 'POLAND'
+    fund_file.parsed_data_file.should == 'pl_in_progress_erdf.csv'
+  end
+  
+  it 'should identify fields from fund_files' do
+    files = fund_files
+    field_names = @loader.field_names(files.first)
+    field_names.first.should == [:beneficiary, :nazwa_beneficjenta] 
+    field_names.second.should == [:project_title, :tytu_projektu]
+    field_names.last.should == [:program_name, :program_operacyjny]
+  end
+=begin  
+  it 'create a record for each row in fund file' do
+    name = 'pl_in_progress_erdf.csv'
+    fund_file = mock('fund_file', :parsed_data_file => name)
+    file_name = RAILS_ROOT+'/data/pl/'+name
+    IO.should_receive(:read).with(file_name).and_return pl_csv
+    @loader.should_receive(:field_names).with(fund_file).and_return [
+    [:beneficiary, :nazwa_beneficjenta],
+    [:project_title, :tytu_projektu],
+    [:program_name, :program_operacyjny]
+    ]
+
+    records = @loader.load_fund_file fund_file
+    records.size.should == 2
+    record = records.first
+    record.beneficiary.should == 'x'
+    record.program_name.should == 'y'
+  end
+=end
+  def fund_files
+    file_name = RAILS_ROOT+'/data/structural_funds_master_scrape_0330_vl.csv'
+    IO.should_receive(:read).with(file_name).and_return csv
+    fund_files = @loader.load_fund_files file_name
+  end
+
+  def pl_csv
+%Q|Nazwa beneficjenta,Tytuł projektu,Program Operacyjny,Działanie,Poddziałanie,Wartość ogółem,Dofinansowanie publiczne,Rok przyznania dofinansowania,Rok wypłacenia ostatniej raty
+""" Enter ""Ośrodek Edukacyjno - Szkoleniowy  Barbara Wolska",Szansa 50+,Program Operacyjny Kapitał Ludzki,7.2. Przeciwdziałanie wykluczeniu i wzmocnienie sektora ekonomii społecznej,7.2.1 Aktywizacja zawodowa i społeczna osób zagrożonych wykluczeniem społecznym,175864.0,174166.93,2008,2009
+"""ARBOS"" Irena Słabolepsza",Rozwój firmy ARBOS poprzez zakup rębaka do drewna,Regionalny Program Operacyjny Województwa Wielkopolskiego na lata 2007 - 2013,Działanie 1.1. Rozwój mikroprzedsiębiorstw,Schemat I: Projekty inwestycyjne,48800.0,21000.0,2009,2009
+|
+  end
+  
+  def pl_csv2
+%Q|"Nazwa beneficjenta","Tytuł projektu","Program Operacyjny","Działanie","Poddziałanie","Wartość ogółem","Dofinansowanie publiczne","Rok przyznania dofinansowania","Rok wypłacenia ostatniej raty"
+""" Enter ""Ośrodek Edukacyjno - Szkoleniowy  Barbara Wolska","Szansa 50+","Program Operacyjny Kapitał Ludzki","7.2. Przeciwdziałanie wykluczeniu i wzmocnienie sektora ekonomii społecznej","7.2.1 Aktywizacja zawodowa i społeczna osób zagrożonych wykluczeniem społecznym",175864,174166.93,2008,2009
+"""ARBOS"" Irena Słabolepsza","Rozwój firmy ARBOS poprzez zakup rębaka do drewna","Regionalny Program Operacyjny Województwa Wielkopolskiego na lata 2007 - 2013","Działanie 1.1. Rozwój mikroprzedsiębiorstw","Schemat I: Projekty inwestycyjne",48800,21000,2009,2009|
+  end
+
+  def csv
+%Q|Country,Region,Assigned to,Excel/PDF,Down-loaded,Scrape Needed,Priority,"Data
+available
+for
+2007","Data
+available
+for
+2008","Data
+available
+for
+2009","Data
+available
+for2010",Program,"Sub-program
+information",Parsed data file,Original file name,Currency Field,Beneficiary Field,Project Title Field,Program Name Field,Amount Allocated Field (EU Funds),Amount Allocated (All funds EU/Nation/Region),Amount Paid Field,Description Field,Year Field,Date Field,Start Year Field,,Direct link to PDF,"Direct link to
+Excel","Direct Link to 
+HTML",Direct link to Doc,,Last Updated,Next update ,Explanatory Notes,Waiting for response,Contact,Uri to landing page,Contact
+POLAND,All regions,,Excel,Done,No,Tier 1,,,,,ERDF,Projects in Progress,pl_in_progress_erdf.csv,Lista_beneficjentow_FE_zakonczone_030110.xls,,Nazwa beneficjenta,Tytu_ projektu,Program Operacyjny,,,,,,,,,http://www.mrr.gov.pl/aktualnosci/fundusze_europejskie_2007_2013/Documents/Lista_beneficjentow_FE_030110.rar
+|
+  end
+
+end
